812b3ecb50141fdc7f31ded5e77ddb97d0c7d3ca
Pete LePage <Pete.LePage@pobox.com>
2012-02-28 18:38:45 -0800

    working

---
+++ ReaderAPI-Notes.md
@@ -0,0 +1,17 @@
+Reader API Details
+
+http://code.google.com/p/pyrfeed/wiki/GoogleReaderAPI
+
+http://mobile.tutsplus.com/tutorials/mobile-web-apps/building-a-mobile-web-application-with-the-google-reader-api/
+
+For logged in user, get all read items
+http://www.google.com/reader/atom/user/-/state/com.google/read
+
+Google Reader exposes feed data as JSON at URLs of the form http://www.google.com/reader/api/0/stream/contents/feed/*escaped_feed_url*, e.g. http://www.google.com/reader/api/0/stream/contents/feed/http%3A//blog.stackoverflow.com/feed/.
+
+http://undoc.in/googlereader.html
+
+
+APIs
+User Info: https://www.google.com/reader/api/0/user-info?output=json
+My Feeds List: https://www.google.com/reader/api/0/subscription/list?output=json
--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ WReader.Item = Em.Object.extend({
   title: null,
   pub_name: null,
   pub_author: null,
-  pub_date: new Date(),
+  pub_date: new Date(0),
   short_desc: null,
   content: null,
   feed_link: null,
@@ -21,6 +21,10 @@ WReader.itemsController = Em.ArrayProxy.create({
     this.pushObject(item);
   },
 
+  read: function() {
+    return this.filterProperty('read', true);
+  }.property('@each.read'),
+
   readCount: function() {
     return this.filterProperty('read', true).get('length');
   }.property('@each.read'),
@@ -32,23 +36,79 @@ WReader.itemsController = Em.ArrayProxy.create({
   starredCount: function() {
     return this.filterProperty('starred', true).get('length');
   }.property('@each.starred')
+
+});
+
+
+WReader.selectedItemController = Em.Object.create({
+  _content: null,
+
+  content: Ember.computed(function(key, value) {
+    if (arguments.length === 1) {
+      //Getter
+      return this.get("_content");
+    } else {
+      //Setter
+      value.set('read', true);
+      this.set("_content", value);
+      //this.set("next", value);
+      return value;
+    }
+  }).property('content')
+});
+
+WReader.ItemListView = Em.View.extend({
+  classNames: ['well', 'summary'],
+  classNameBindings: ['active', 'read'],
+  /*contentBinding: this,*/
+  contentBinding: 'WReader.itemsController.content',
+  click: function(evt) {
+    var content = this.get('content');
+    WReader.selectedItemController.set('content', content);
+  },
+  active: function() {
+    var selectedItem = WReader.selectedItemController.get('content'),
+      content = this.get('content');
+    if (content === selectedItem) { return true; }
+  }.property('WReader.selectedItemController.content'),
+  read: function() {
+    var read = this.get('content').get('read');
+    return read;
+  }.property('WReader.selectedItemController.content'),
+  formattedDate: function() {
+    var date = this.get('content').get('pub_date');
+    return moment(date).format("MMMM Do YYYY, h:mm a");
+  }.property('WReader.selectedItemController.content')
+});
+
+WReader.ItemView = Em.View.extend({
+  classNames: ['well', 'entry'],
+  contentBinding: 'WReader.selectedItemController.content',
+  formattedDate: function() {
+    var date = this.get('content').get('pub_date');
+    return moment(date).format("dddd, MMMM Do YYYY, h:mm a");
+  }.property('WReader.selectedItemController.content'),
+  keyUp: function(evt) {
+    console.log('key', evt);
+  }
 });
 
 /*
  Helper test methods to add a couple of items to our list
 */
 var item = WReader.Item.create({ item_id: 1, title: 'Item A', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(), sort_desc: 'Short Desc',
+    pub_author: 'Author', pub_date: new Date(0), short_desc: 'Short Desc',
     content: 'Lorem Ipsum, yah it\'s all that!', starred: true,
     feed_link: 'http://google.com/', item_link: 'http://google.com/1'});
 WReader.itemsController.addItem(item);
 item = WReader.Item.create({ item_id: 2, title: 'Item B', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(), sort_desc: 'Short Desc',
+    pub_author: 'Author', pub_date: new Date(5000000000), short_desc: 'Short Desc',
     content: 'Lorem Ipsum, yah it\'s all that!', starred: false,
     feed_link: 'http://google.com/', item_link: 'http://google.com/2'});
 WReader.itemsController.addItem(item);
 item = WReader.Item.create({ item_id: 3, title: 'Born Read', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(), sort_desc: 'Short Desc',
+    pub_author: 'Author', pub_date: new Date(1000000000000), short_desc: 'Short Desc',
     content: 'Lorem Ipsum, yah it\'s all that!', starred: false, read: true,
     feed_link: 'http://google.com/', item_link: 'http://google.com/3'});
 WReader.itemsController.addItem(item);
+
