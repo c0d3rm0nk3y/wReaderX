aec56cffe363055075a184c4edb8558fdfa75d40
Pete LePage <Pete.LePage@pobox.com>
2012-03-01 16:45:18 -0800

    more

--- js/app.js
+++ js/app.js
@@ -35,80 +35,116 @@ WReader.itemsController = Em.ArrayProxy.create({
 
   starredCount: function() {
     return this.filterProperty('starred', true).get('length');
-  }.property('@each.starred')
+  }.property('@each.starred'),
+  
+  select: function(item) {
+    item.set('read', true);
+    this.set('selectedItem', item);
+  },
+  selectedItem: null,
 
-});
-
-
-WReader.selectedItemController = Em.Object.create({
-  _content: null,
-
-  content: Ember.computed(function(key, value) {
-    if (arguments.length === 1) {
-      //Getter
-      return this.get("_content");
-    } else {
-      //Setter
-      value.set('read', true);
-      this.set("_content", value);
-      //this.set("next", value);
-      return value;
+  next: function() {
+    var currentIndex = this.content.indexOf(this.get('selectedItem'));
+    var nextItem = this.content[currentIndex + 1];
+    if (nextItem) {
+      this.select(nextItem);
     }
-  }).property('content')
+  },
+
+  prev: function() {
+    var currentIndex = this.content.indexOf(this.get('selectedItem'));
+    var nextItem = this.content[currentIndex - 1];
+    if (nextItem) {
+      this.select(nextItem);
+    }
+  }
 });
 
-WReader.ItemListView = Em.View.extend({
+WReader.SummaryListView = Em.View.extend({
   classNames: ['well', 'summary'],
-  classNameBindings: ['active', 'read'],
-  /*contentBinding: this,*/
-  contentBinding: 'WReader.itemsController.content',
+  classNameBindings: ['active', 'read', 'prev', 'next'],
   click: function(evt) {
     var content = this.get('content');
-    WReader.selectedItemController.set('content', content);
+    WReader.itemsController.select(content);
   },
   active: function() {
-    var selectedItem = WReader.selectedItemController.get('content'),
-      content = this.get('content');
-    if (content === selectedItem) { return true; }
-  }.property('WReader.selectedItemController.content'),
+    var selectedItem = WReader.itemsController.get('selectedItem');
+    var content = this.get('content');
+    if (content === selectedItem) {
+      return true;
+    }
+  }.property('WReader.itemsController.selectedItem'),
   read: function() {
     var read = this.get('content').get('read');
     return read;
-  }.property('WReader.selectedItemController.content'),
+  }.property('WReader.itemsController.@each.read'),
   formattedDate: function() {
     var date = this.get('content').get('pub_date');
     return moment(date).format("MMMM Do YYYY, h:mm a");
-  }.property('WReader.selectedItemController.content')
+  }.property('WReader.itemsController.selectedItem')
 });
 
-WReader.ItemView = Em.View.extend({
+WReader.EntryListView = Em.View.extend({
   classNames: ['well', 'entry'],
-  contentBinding: 'WReader.selectedItemController.content',
+  classNameBindings: ['active', 'read', 'prev', 'next'],
+  active: function() {
+    var selectedItem = WReader.itemsController.get('selectedItem');
+    var content = this.get('content');
+    if (content === selectedItem) {
+      return true;
+    }
+  }.property('WReader.itemsController.selectedItem'),
+  read: function() {
+    var read = this.get('content').get('read');
+    return read;
+  }.property('WReader.itemsController.@each.read'),
   formattedDate: function() {
     var date = this.get('content').get('pub_date');
-    return moment(date).format("dddd, MMMM Do YYYY, h:mm a");
-  }.property('WReader.selectedItemController.content'),
-  keyUp: function(evt) {
-    console.log('key', evt);
-  }
+    return moment(date).format("MMMM Do YYYY, h:mm a");
+  }.property('WReader.itemsController.selectedItem')
 });
+
+function handleBodyKeyDown(evt) {
+  switch (evt.keyCode) {
+    case 34: // PgDn
+    case 39: // right arrow
+    case 40: // down arrow
+    case 74: // j
+      WReader.itemsController.next();
+      event.preventDefault();
+      break;
+
+    case 33: // PgUp
+    case 37: // left arrow
+    case 38: // up arrow
+    case 75: // k
+      WReader.itemsController.prev();
+      event.preventDefault();
+      break;
+    }
+}
+
+document.addEventListener('keydown', handleBodyKeyDown, false);
+
+
 
 /*
  Helper test methods to add a couple of items to our list
 */
-var item = WReader.Item.create({ item_id: 1, title: 'Item A', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(0), short_desc: 'Short Desc',
-    content: 'Lorem Ipsum, yah it\'s all that!', starred: true,
-    feed_link: 'http://google.com/', item_link: 'http://google.com/1'});
-WReader.itemsController.addItem(item);
-item = WReader.Item.create({ item_id: 2, title: 'Item B', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(5000000000), short_desc: 'Short Desc',
-    content: 'Lorem Ipsum, yah it\'s all that!', starred: false,
-    feed_link: 'http://google.com/', item_link: 'http://google.com/2'});
-WReader.itemsController.addItem(item);
-item = WReader.Item.create({ item_id: 3, title: 'Born Read', pub_name: 'Feed',
-    pub_author: 'Author', pub_date: new Date(1000000000000), short_desc: 'Short Desc',
-    content: 'Lorem Ipsum, yah it\'s all that!', starred: false, read: true,
-    feed_link: 'http://google.com/', item_link: 'http://google.com/3'});
-WReader.itemsController.addItem(item);
-
+for (var i = 0; i < 20; i++) {
+  var item = WReader.Item.create({ item_id: 1, title: 'Item A',
+    pub_name: 'Feed', pub_author: 'Author', pub_date: new Date(1000000000000),
+    short_desc: 'Short Desc', content: 'Lorem Ipsum, yah it\'s all that!',
+    feed_link: 'http://google.com/', item_link: 'http://google.com/'});
+  item.item_id = i;
+  item.title += " " + i.toString();
+  item.item_link += i.toString();
+  item.pub_date = new Date(1300000000000 + i * 86400000 + (86400000 * Math.random()));
+  if (Math.random() > 0.5) {
+    item.read = true;
+  }
+  if (Math.random() > 0.5) {
+    item.starred = true;
+  }
+  WReader.itemsController.addItem(item);
+}
