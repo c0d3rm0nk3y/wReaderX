dbad672c2269d49d24239752493a07d3bac88b94
Pete LePage <Pete.LePage@pobox.com>
2012-03-02 23:17:00 -0800

    .

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-var WReader = Em.Application.create();
+var WReader = Em.Application.create({
+  ready: function() {
+    WReader.settingsController.loadSettings();
+    //WReader.itemsController.loadItems();
+    WReader.GetItemsFromDataStore();
+    WReader.GetItemsFromServer();
+  }
+});
+
+WReader.GetItemsFromDataStore = function() {
+
+};
+
+WReader.GetItemsFromServer = function() {
+  $.ajax({
+    url: 'fake-data.json',
+    dataType: 'json',
+    success: function(data) {
+      var items = data.map(function(obj) {
+        var item = WReader.Item.create(obj);
+        WReader.itemsController.addItem(item);
+      });
+    },
+
+    error: function() {
+
+    }
+  });
+};
+
+WReader.GetItemsFromServer();
+
+WReader.Settings = Em.Object.extend({
+  tabletControls: false
+});
+
+WReader.settingsController = WReader.Settings.create({
+  loadSettings: function() {
+
+  },
+  saveSettings: function() {
+
+  }
+});
 
 WReader.Item = Em.Object.extend({
   read: false,
@@ -14,16 +57,39 @@ WReader.Item = Em.Object.extend({
   item_link: null
 });
 
-WReader.itemsController = Em.ArrayProxy.create({
+WReader.itemsController = Em.ArrayController.create({
   content: [],
 
   addItem: function(item) {
-    this.pushObject(item);
+    var exists = this.filterProperty('item_id', item.item_id).length;
+    if (!exists) {
+      var length = this.get('length'), idx;
+      idx = this.binarySearch(Date.parse(item.get('pub_date')), 0, length);
+      this.insertAt(idx, item);
+    }
   },
 
-  read: function() {
-    return this.filterProperty('read', true);
-  }.property('@each.read'),
+  // Binary search implementation that finds the index where a entry
+  // should be inserted when sorting by date.
+  binarySearch: function(value, low, high) {
+    var mid, midValue;
+    if (low === high) {
+      return low;
+    }
+    mid = low + Math.floor((high - low) / 2);
+    midValue = Date.parse(this.objectAt(mid).get('pub_date'));
+
+    if (value < midValue) {
+      return this.binarySearch(value, mid + 1, high);
+    } else if (value > midValue) {
+      return this.binarySearch(value, low, mid);
+    }
+    return mid;
+  },
+
+  itemCount: function() {
+    return this.get('length');
+  }.property('@each'),
 
   readCount: function() {
     return this.filterProperty('read', true).get('length');
@@ -36,7 +102,23 @@ WReader.itemsController = Em.ArrayProxy.create({
   starredCount: function() {
     return this.filterProperty('starred', true).get('length');
   }.property('@each.starred'),
-  
+
+  loadItems: function() {
+    $.ajax({
+      url: 'fake-data.json',
+      dataType: 'json',
+      success: function(data) {
+        var items = data.map(function(obj) {
+          var item = WReader.Item.create(obj);
+          WReader.itemsController.addItem(item);
+        });
+      },
+
+      error: function() {
+
+      }
+    });
+  }
 });
 
 WReader.selectedItemController = Em.Object.create({
@@ -85,17 +167,26 @@ WReader.selectedItemController = Em.Object.create({
     if (nextItem) {
       this.select(nextItem);
     }
+  },
+
+  scrollAndNext: function() {
+    console.log('scroll first');
+    this.next();
   }
 });
 
 WReader.selectedItemController.addObserver('selectedItem', function() {
   //TODO: could change to use document.querySelector
+  /*
   var curScrollPos = $('.entries').scrollTop();
   var itemTop = $('.entry.active').offset().top - 60;
   $(".entries").animate({"scrollTop": curScrollPos + itemTop}, 200);
+  */
 
-  curScrollPos = $('.summaries').scrollTop();
-  itemTop = $('.summary.active').offset().top - 60;
+  $('.entries').animate({"scrollTop": 0}, 200);
+
+  var curScrollPos = $('.summaries').scrollTop();
+  var itemTop = $('.summary.active').offset().top - 60;
   $(".summaries").animate({"scrollTop": curScrollPos + itemTop}, 200);
 });
 
@@ -103,6 +194,7 @@ WReader.SummaryListView = Em.View.extend({
   tagName: 'article',
   classNames: ['well', 'summary'],
   classNameBindings: ['active', 'read', 'prev', 'next'],
+  contentBinding: 'WReader.itemsController.content',
   click: function(evt) {
     var content = this.get('content');
     WReader.selectedItemController.select(content);
@@ -124,39 +216,13 @@ WReader.SummaryListView = Em.View.extend({
   }.property('WReader.selectedItemController.selectedItem')
 });
 
-WReader.EntryListView = Em.View.extend({
-  tagName: 'article',
-  classNames: ['well', 'entry'],
-  classNameBindings: ['active', 'read', 'prev', 'next'],
-  active: function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    var content = this.get('content');
-    if (content === selectedItem) {
-      return true;
-    }
-  }.property('WReader.selectedItemController.selectedItem'),
-  read: function() {
-    var read = this.get('content').get('read');
-    return read;
-  }.property('WReader.itemsController.@each.read'),
-  formattedDate: function() {
-    var date = this.get('content').get('pub_date');
-    var result = moment(date).format("MMMM Do YYYY, h:mm a");
-    result += " (" + moment(date).fromNow() + ")";
-    return result;
-  }.property('WReader.selectedItemController.selectedItem')
-});
-
 WReader.EntryItemView = Em.View.extend({
   tagName: 'article',
+  contentBinding: 'WReader.selectedItemController.selectedItem',
   classNames: ['well', 'entry'],
   classNameBindings: ['active', 'read', 'prev', 'next'],
   active: function() {
-    var selectedItem = WReader.selectedItemController.get('selectedItem');
-    var content = this.get('content');
-    if (content === selectedItem) {
-      return true;
-    }
+    return true;
   }.property('WReader.selectedItemController.selectedItem'),
   read: function() {
     var read = this.get('content').get('read');
@@ -171,6 +237,12 @@ WReader.EntryItemView = Em.View.extend({
 });
 
 WReader.NavControlsView = Em.View.extend({
+  tagName: 'section',
+  classNames: ['controls'],
+  classNameBindings: ['hide'],
+  hide: function() {
+    return !WReader.settingsController.tabletControls;
+  }.property('WReader.settingsController.tabletControls'),
   navUp: function(event) {
     WReader.selectedItemController.prev();
   },
@@ -210,7 +282,28 @@ WReader.NavControlsView = Em.View.extend({
   }.property('WReader.selectedItemController.selectedItem')
 });
 
+function boo() {
+  
+
+}
+
+WReader.HandleSpaceKey = function() {
+  var itemHeight = $('.entry.active').height() + 60;
+  var winHeight = $(window).height();
+  var curScroll = $('.entries').scrollTop();
+  var scroll = curScroll + winHeight;
+  console.log(scroll, itemHeight);
+  if (scroll < itemHeight) {
+    $('.entries').scrollTop(scroll);
+  } else {
+    WReader.selectedItemController.next();
+  }
+};
+
 function handleBodyKeyDown(evt) {
+  
+  //console.log("X", evt.keyCode);
+
   switch (evt.keyCode) {
     case 34: // PgDn
     case 39: // right arrow
@@ -219,11 +312,28 @@ function handleBodyKeyDown(evt) {
       WReader.selectedItemController.next();
       break;
 
+    case 32: // Space
+      WReader.HandleSpaceKey();
+      evt.preventDefault();
+      break;
+
     case 33: // PgUp
     case 37: // left arrow
     case 38: // up arrow
     case 75: // k
       WReader.selectedItemController.prev();
+      break;
+
+    case 85: // U
+      WReader.selectedItemController.toggleRead();
+      break;
+
+    case 72: // H
+      WReader.selectedItemController.toggleStar();
+      break;
+
+    case 67: // C
+      WReader.settingsController.set('tabletControls', !WReader.settingsController.tabletControls);
       break;
     }
 }
@@ -239,20 +349,64 @@ window.addEventListener('popstate', handlePopState, false);
 /*
  Helper test methods to add a couple of items to our list
 */
-for (var i = 0; i < 101; i++) {
-  var item = WReader.Item.create({ item_id: 1, title: 'Item A',
-    pub_name: 'Feed', pub_author: 'Author', pub_date: new Date(1000000000000),
-    short_desc: 'Short Desc', content: 'Lorem Ipsum, yah it\'s all that!',
-    feed_link: 'http://google.com/', item_link: 'http://google.com/'});
+//var items = [];
+
+  var names = ["Adam", "Bert", "Charlie", "Dave", "Ernie", "Frances",
+    "Gary", "Isabelle", "John", "Kyle", "Lyla", "Matt", "Nancy", "Ophelia",
+    "Peter", "Quentin", "Rachel", "Stan", "Tom", "Uma", "Veronica", "Wilson",
+    "Xander", "Yehuda", "Zora"];
+  var feeds = ["Engadget", "Gizmodo", "Memegen", "New York Times"];
+
+  var lorem = "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed ";
+  lorem += "do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ";
+  lorem += "ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ";
+  lorem += "aliquip ex ea commodo consequat. Duis aute irure dolor in ";
+  lorem += "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla ";
+  lorem += "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in ";
+  lorem += "culpa qui officia deserunt mollit anim id est laborum.</p>";
+  lorem += lorem;
+  lorem += lorem;
+  lorem += lorem;
+
+function addNewItems(num) {
+  for (var i = 0; i < num; i++) {
+    var item = {};
+    item.item_id = i;
+    item.pub_name = feeds[Math.floor(Math.random()*feeds.length)];
+    item.pub_author = names[Math.floor(Math.random()*names.length)] + " " + names[Math.floor(Math.random()*names.length)];
+    item.title = "Item Title " + i.toString();
+    item.item_link = "http://url/" + i.toString();
+    item.feed_link = "http://url/" + i.toString();
+    item.content = "<p>" + item.title + "<p>" + lorem;
+    item.short_desc = item.content.substr(0, 128) + "...";
+    item.pub_date = new Date(1300000000000 + i * 86400000 + (86400000 * Math.random()));
+    if (Math.random() > 0.5) {
+      item.read = true;
+    }
+    if (Math.random() > 0.5) {
+      item.starred = true;
+    }
+    //items.push(item);
+    item = WReader.Item.create(item);
+    WReader.itemsController.addItem(item);
+  }
+}
+
+function addSingleNew() {
+  var item = {};
+  var i = 100;
   item.item_id = i;
-  item.title += " " + i.toString();
-  item.item_link += i.toString();
-  item.pub_date = new Date(1300000000000 + i * 86400000 + (86400000 * Math.random()));
-  if (Math.random() > 0.5) {
-    item.read = true;
-  }
-  if (Math.random() > 0.5) {
-    item.starred = true;
-  }
+  item.pub_name = feeds[Math.floor(Math.random()*feeds.length)];
+  item.pub_author = names[Math.floor(Math.random()*names.length)] + " " + names[Math.floor(Math.random()*names.length)];
+  item.title = "Item Title " + i.toString();
+  item.item_link = "http://url/" + i.toString();
+  item.feed_link = "http://url/" + i.toString();
+  item.content = "<p>" + item.title + "<p>" + lorem;
+  item.short_desc = item.content.substr(0, 128) + "...";
+  item.pub_date = new Date(1100000000000 + i * 86400000 + (86400000 * Math.random()));
+  item.read = false;
+  item = WReader.Item.create(item);
   WReader.itemsController.addItem(item);
 }
+
+
